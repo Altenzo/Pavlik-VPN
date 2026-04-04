d74eacaad796424105b57e3f1b41ece5db9bf56d
BlagoVpn <sv809991@gmail.com>
2026-04-05 00:02:53 +0300

    Update add_vpn_uuid.py

--- migrations/versions/add_vpn_uuid.py
+++ migrations/versions/add_vpn_uuid.py
@@ -1,7 +1,7 @@
-"""add vpn_uuid to users
+"""add vpn_uuid and vless_link to users
 
 Revision ID: a1b2c3d4e5f6
-Revises: 7a7b7c7d7e7f
+Revises: 1de4c83c38e4
 Create Date: 2026-04-01 00:00:00.000000
 """
 from typing import Sequence, Union
@@ -16,7 +16,9 @@ depends_on = None
 
 def upgrade() -> None:
     op.add_column('users', sa.Column('vpn_uuid', sa.String(length=64), nullable=True))
+    op.add_column('users', sa.Column('vless_link', sa.String(length=1024), nullable=True))
 
 
 def downgrade() -> None:
+    op.drop_column('users', 'vless_link')
     op.drop_column('users', 'vpn_uuid')
